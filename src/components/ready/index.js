4ba860f9ffc380e89437ffb2daa4d170da6bb930
Abolare Roheemah <roheemahabo@gmail.com>
2023-05-08 13:01:51 +0100

    ready section

---
+++ src/components/ready/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ready.css'
+
+export default function Ready() {
+  return (
+    <div className='ready-ctn'>
+        {/* <img src="../../public/assets/images/pngs/ready.png" alt="" /> */}
+    </div>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Jumbotron from '../../components/Jumbotron';
+import Ready from '../../components/ready';
 import './home.css';
 
 export default function Home() {
   return (
     <div>
         <Jumbotron />
+        <Ready />
     </div>
   )
 }
