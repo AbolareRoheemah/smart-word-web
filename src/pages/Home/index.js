44aee97f5a5de05c93463402a7b4404bc1765093
Jst-Deoda <divineattah293@gmai.com>
2023-05-12 15:13:06 +0100

    task done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import Header from '../../components/Header';
-import About from '../../components/About';
-import Jumbotron from '../../components/Jumbotron';
-import Levels from '../../components/Levels';
-import Ready from '../../components/ReadySection';
-import Footer from '../../components/Footer';
-import './home.css';
+import React from "react";
+import Header from "../../components/Header";
+import About from "../../components/About";
+import Jumbotron from "../../components/Jumbotron";
+import Levels from "../../components/Levels";
+import PlayforFree from "../../components/PlayForFree/PlayforFree";
+import PlaytoEarn from "../../components/PlayToEarn/PlaytoEarn";
+import Ready from "../../components/ReadySection";
+import Footer from "../../components/Footer";
+import "./home.css";
 
 export default function Home() {
   return (
-    <div className='home-ctn'>
-        <Header />
-        <Jumbotron />
-        <About />
-        <Levels />
-        <Ready />
+    <div className="home-ctn">
+      <Header />
+      <Jumbotron />
+      <About />
+      <Levels />
+      <PlayforFree />
+      <PlaytoEarn />
+      <Ready />
       <Footer />
     </div>
-  )
+  );
 }
